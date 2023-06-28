8d82b6022f8b9797ed648b535401ca932f0369f0
Naincy <naincyk15042004@gmail.com>
2023-06-28 23:36:53 +0530

    Integrated the API and built the Homepage

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import { Link } from "react-router-dom";
 const Sidebar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
 
   //Early return
   if (!isMenuOpen) return null;
-  
+
   return (
     <div className="p-4 shadow-lg w-48">
       <ul>
-        <li>Home</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
         <li>Sports</li>
         <li>Videos</li>
         <li>Live</li>
-        <li></li>
+        
       </ul>
       <h1 className="font-bold pt-5">Subscriptions</h1>
       <ul>
---
+++ src/utils/contants.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY ="AIzaSyAjaVwOyBZjyrTYPMhIMSzt76hI2_J8jc8";
+
+export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
