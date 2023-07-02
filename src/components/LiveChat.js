c5eec0e052e8699c79c1ece71508439df892b651
Naincy <naincyk15042004@gmail.com>
2023-07-02 12:54:52 +0530

    Adding random names and messages to our livechat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,22 +1,39 @@
 import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomName, makeRandomMessage } from "../utils/helper";
+
 
 const LiveChat = () => {
+  
 
-useEffect(()=>{
-    const i = setInterval(()=>{
-        //API Polling
-    },2000);
-    return ()=> clearInterval(i);
-},[]);
+  const dispatch = useDispatch();
+  const chatMessages = useSelector((store)=>store.chat.messages);
+  useEffect(() => {
+    const i = setInterval(() => {
+      //API Polling
+// console.log("API");
 
+dispatch(addMessage({
+  name:generateRandomName(),
+  message:makeRandomMessage(15)+"🙂",
+})
+);
+
+    }, 500);
+    return () => clearInterval(i);
+  }, []);
 
   return (
-  <div className="border border-black ml-2 p-2 h-[480px] w-full bg-slate-50 rounded-lg">
-<ChatMessage name="Lakshya kumar" message="Good Vibes😍"/>
-
+    <div className="border border-black ml-2 p-2 h-[480px] w-full bg-slate-50 rounded-lg overflow-y-scroll flex flex-col-reverse">
+      {chatMessages.map((c,i)=>(
+        // don't use indexes as keys 
+      <ChatMessage key={i} name={c.name} message={c.message}/>
+      ))
+      }
     </div>
-  )
-}
+  );
+};
 
 export default LiveChat;
--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -1,4 +1,5 @@
  import { createSlice } from "@reduxjs/toolkit";
+ 
  const chatSlice = createSlice({
  name:"chat",
  initialState:{
@@ -6,7 +7,9 @@
  },
  reducers:{
     addMessage:(state,action)=>{
-    state.messages.push(action.payload);
+      
+      state.messages.splice(10,1);
+    state.messages.unshift(action.payload);
     },
  },
  });
